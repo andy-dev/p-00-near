664911cd2fb67ad5ca605c5f3470f6cf4139efd7
Andres Macedo <andres.macedo@gmail.com>
2021-09-27 19:59:29 -0500

    removed un used test

--- src/Scores/__tests__/index.unit.spec.ts
+++ src/Scores/__tests__/index.unit.spec.ts
@@ -1,25 +1,24 @@
 import { Contract } from "../assembly";
 
-let contract: Contract
+let contract: Contract;
 
 beforeEach(() => {
-  contract = new Contract()
-})
+  contract = new Contract();
+});
 
 describe("Contract", () => {
   // VIEW method tests
 
-  it("says hello", () => {
-    expect(contract.helloWorld()).toStrictEqual("hello world")
-  })
-
   it("reads data", () => {
-    expect(contract.read("some key")).toStrictEqual("🚫 Key [ some key ] not found in storage. ( storage [ 0 bytes ] )")
-  })
+    expect(contract.read("some key")).toStrictEqual(
+      "🚫 Key [ some key ] not found in storage. ( storage [ 0 bytes ] )"
+    );
+  });
 
   // CHANGE method tests
-
   it("saves data to contract storage", () => {
-    expect(contract.write("some-key", "some value")).toStrictEqual("✅ Data saved. ( storage [ 18 bytes ] )")
-  })
-})
+    expect(contract.write("some-key", "some value")).toStrictEqual(
+      "✅ Data saved. ( storage [ 18 bytes ] )"
+    );
+  });
+});
